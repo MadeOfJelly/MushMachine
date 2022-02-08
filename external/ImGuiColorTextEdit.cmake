170b21089f972e9de865a2c744c7a5b6980cf7b2
Green Sky <green@g-s.xyz>
2022-02-08 19:36:31 +0100

    force c++11 on text editor

--- external/ImGuiColorTextEdit.cmake
+++ external/ImGuiColorTextEdit.cmake
@@ -4,6 +4,7 @@ add_library(imgui_color_text_edit
 	"${CMAKE_CURRENT_LIST_DIR}/ImGuiColorTextEdit/TextEditor.h"
 	"${CMAKE_CURRENT_LIST_DIR}/ImGuiColorTextEdit/TextEditor.cpp"
 )
+target_compile_features(imgui_color_text_edit PUBLIC cxx_std_11)
 target_include_directories(imgui_color_text_edit PUBLIC "${CMAKE_CURRENT_LIST_DIR}/ImGuiColorTextEdit")
 target_link_libraries(imgui_color_text_edit
 	imgui
