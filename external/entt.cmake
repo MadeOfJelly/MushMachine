7237cb265d4f8aaa06af776c7902bb56f602d252
Green Sky <Green-Sky@users.noreply.github.com>
2025-05-21 12:04:08 +0200

    cmake version upgrade

--- external/entt.cmake
+++ external/entt.cmake
@@ -1,4 +1,4 @@
-cmake_minimum_required(VERSION 3.9 FATAL_ERROR)
+cmake_minimum_required(VERSION 3.9...3.16 FATAL_ERROR)
 add_library(entt INTERFACE)
 target_include_directories(entt INTERFACE "${CMAKE_CURRENT_LIST_DIR}/entt/src")
 target_compile_features(entt INTERFACE cxx_std_17)
